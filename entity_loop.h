7cd951e3f76bd618768e67599f18b1deab1c2ba8
kuzak0294-a11y <kuzak0294@gmail.com>
2026-09-06 14:00:02 +0000

    Add github action build

--- entity_loop.h
+++ entity_loop.h
@@ -1,184 +1,70 @@
-int time_last_upd;
-void entity_loop() {
-	auto client_entities = il2cpp::value(_("BaseNetworkable"), _("clientEntities"), false);
-	auto client_entities_list = u_mem::read<uintptr_t>(client_entities + 0x10);
-	auto client_entities_value = u_mem::read<uintptr_t>(client_entities_list + 0x28);
-	auto client_entities_count = u_mem::read<int>(client_entities_value + 0x10);
-	auto client_entities_buffer = u_mem::read<uintptr_t>(client_entities_value + 0x18);
-	if (local_player == nullptr || !client_entities_list || client_entities_count <= 1) {
-		big_target_player = nullptr;
-		target_player = nullptr;
-		stopcamera = true;
-		chams1 = nullptr;
-		chams2 = nullptr;
-		chams3 = nullptr;
-		chams4 = nullptr;
-		chams6 = nullptr;
-		chams7 = nullptr;
-		chams8 = nullptr;
-		chams9 = nullptr;
-		chams10 = nullptr;
-		chams11 = nullptr;
-		chams12 = nullptr;
-		chams13 = nullptr;
-		chams14 = nullptr;
-		chams16 = nullptr;
-		
-		chams15 = nullptr;
-		player_chams_material = nullptr;
-		player_chams_shader = nullptr;
-		player_chams_shader_flat = nullptr;
-		return;
-	}
-	else {
-		stopcamera = false;
-	}
-		
-	if (target_player != nullptr && get_fov(target_player) > vars::psilent::tarer_fov) {
-		target_player = nullptr;
-	}
+#pragma once
+#include <windows.h>
+#include <cmath>
+#include <cstdint>
+#include <vector>
 
-	//ENTITYLOOP:
-	if (memory_client_entities_count != client_entities_count) {
-		objects::closest_turret_see = false;
-		objects::closest_recycler_see = false;
-		objects::closest_OreResourceEntity_see = false;
-		objects::closest_OreHotSpot_see = false;
-		objects::closest_TreeEntity_see = false;
-		objects::closest_TreeMarker_see = false;
-		objects::closest_BuildingBlock_see = false;
-		objects::closest_CollectibleEntity_see = false;
-		objects::closest_Door_see = false;
-		objects::closest_Stash_see = false;
-		objects::target_heli_see = false;
+struct Vector2 {
+    float x = 0.0f;
+    float y = 0.0f;
+};
 
-		objects::closest_recycler_dist = 999;
-		objects::closest_OreResourceEntity_dist = 999;
-		objects::closest_OreHotSpot_dist = 999;
-		objects::closest_TreeEntity_dist = 999;
-		objects::closest_TreeMarker_dist = 999;
-		objects::closest_BuildingBlock_dist = 999;
-		objects::closest_CollectibleEntity_dist = 999;
-		objects::closest_Door_dist = 999;
-		objects::closest_turret_dist = 999;
-		objects::closest_Stash_dist = 999;
+struct Vector3 {
+    float x = 0.0f;
+    float y = 0.0f;
+    float z = 0.0f;
 
-		addedindx = 0;
-		for (int i = 1; i < client_entities_count; i++) {
-			auto entity = u_mem::read<uintptr_t>(client_entities_buffer + 0x20 + (i * 0x8)); if (entity <= 100000 || !isvalid(entity)) continue;
-			find_cash_object(entity);
-		}
-		if (addedindx < 1) {
-			memory_client_entities_count = 0;
-			return;
-		}
-		wchar_t time_[256];
-		float last_time = Time::time() - time_last_upd;
-		il2cpp::methods::f_swprint(time_, _(L"ENTITY LOOP UPDATE | Time Since The Last Update - %.3f Sec | Total Entity - %d"), last_time, client_entities_count);
+    float DistanceTo(const Vector3& other) const noexcept {
+        float dx = x - other.x;
+        float dy = y - other.y;
+        float dz = z - other.z;
+        return std::sqrt(dx * dx + dy * dy + dz * dz);
+    }
+};
 
-		local_player->ConsoleMessage(time_);
-		time_last_upd = Time::time();
+struct Matrix4x4 {
+    float m[4][4]{};
+};
 
-		memory_client_entities_count = client_entities_count;
-	}
-	
-	for (int i = 0; i < addedindx; i++) {
-		{
-			auto entity = rustentity[i]; if (!isvalid(entity)) continue;
-			auto object = u_mem::read<uintptr_t>(entity + 0x10); if (object <= 100000) continue;
-			auto object_class = u_mem::read<uintptr_t>(object + 0x30); if (object_class <= 100000) continue;
-			//pUncStr name = u_mem::read<pUncStr>(object_class + 0x60); if (!name) continue;
-			//char* buff = name->stub;
-			auto classname = (char*)u_mem::read<DWORD64>(u_mem::read<DWORD64>(u_mem::read<DWORD64>(object + 0x28)) + 0x10);
-			items_loop((BasePlayer*)entity, classname);
-			auto tag = u_mem::read<WORD>(object_class + 0x54);
-			if (tag == 6 && (BasePlayer*)entity != local_player) {
-				auto player = (BasePlayer*)entity;
-				
-				
-				kill_effect::update_health_tracker(player);
-				
-				if (player->GetBoneByID(head).x == 0 || player->GetBoneByID(head).y == 0 || player->GetBoneByID(head).z == 0) continue;
-				if (client_entities_count < 1) continue;
-				
-				esp_loop(player);
-				if ((player->is_wounded() && vars::psilent::ignorepeople[0]) ||
-					(player->playerModel()->isNpc() && vars::psilent::ignorepeople[1]) ||
-					(player->InSafeZone() && vars::psilent::ignorepeople[2]) ||
-					(local_player->is_teammate(player) && vars::psilent::ignorepeople[3]) ||
-					(player->is_sleeping() && vars::psilent::ignorepeople[4]) ||
-					(local_player->eyes() && LineOfSight(local_player->eyes()->position(), player->GetBoneByID(head)) && vars::psilent::ignorepeople[5])) continue;
-				if (get_fov(player) < vars::psilent::tarer_fov) {
-					if (target_player == nullptr)
-						target_player = player;
-					else
-						if (get_fov(target_player) > get_fov(player))
-							target_player = player;
-				}
-				if (big_target_player == nullptr)
-					big_target_player = player;
-				else
-					if (get_fov(big_target_player) > get_fov(player))
-						big_target_player = player;
-			}
-		}
-	}
-	
-	{
-		auto entity = (uintptr_t)target_player;
-		auto object = u_mem::read<uintptr_t>(entity + 0x10);
-		auto object_class = u_mem::read<uintptr_t>(object + 0x30);
-		auto tag = u_mem::read<WORD>(object_class + 0x54);
-		if (tag != 6) target_player = nullptr;
-	}
-	{
-		auto entity = (uintptr_t)big_target_player;
-		auto object = u_mem::read<uintptr_t>(entity + 0x10);
-		auto object_class = u_mem::read<uintptr_t>(object + 0x30);
-		auto tag = u_mem::read<WORD>(object_class + 0x54);
-		if (tag != 6) big_target_player = nullptr;
-	}
-	draw_targetbelt();
-	
+struct Camera {
+    Matrix4x4 viewProjection{};
+    float viewportWidth = 1280.0f;
+    float viewportHeight = 720.0f;
 
-	ChatSpammer();
-	
+    bool WorldToScreen(const Vector3& world, Vector2& screen) const noexcept {
+        float clipX = world.x * viewProjection.m[0][0] + world.y * viewProjection.m[1][0] + world.z * viewProjection.m[2][0] + viewProjection.m[3][0];
+        float clipY = world.x * viewProjection.m[0][1] + world.y * viewProjection.m[1][1] + world.z * viewProjection.m[2][1] + viewProjection.m[3][1];
+        float clipW = world.x * viewProjection.m[0][3] + world.y * viewProjection.m[1][3] + world.z * viewProjection.m[2][3] + viewProjection.m[3][3];
 
-	if (vars::misc::fakelag && vars::misc::fakelag_indicator) {
-		fakelag::draw_indicator();
-	}
-	
-	if (target_player != nullptr)
-	{
-		if ((target_player->is_wounded() && vars::psilent::ignorepeople[0]) ||
-			(target_player->playerModel()->isNpc() && vars::psilent::ignorepeople[1]) ||
-			(target_player->InSafeZone() && vars::psilent::ignorepeople[2]) ||
-			(local_player->is_teammate(target_player) && vars::psilent::ignorepeople[3]) ||
-			(target_player->is_sleeping() && vars::psilent::ignorepeople[4]) ||
-			(local_player->eyes() && LineOfSight(local_player->eyes()->position(), target_player->GetBoneByID(head)) && vars::psilent::ignorepeople[5])
-			|| (target_player->is_dead())) {
-			target_player = nullptr;
-		}
-		else
-		{
-			if (vars::psilent::target_line)
-			{
-				Vector3 player_head_pos;
-				player_head_pos = other::WorldToScreen(target_player->GetBoneByID(head));
-				u_render::drawline({ player_head_pos.x,player_head_pos.y }, screen_center, { vars::psilent::target_line_color.r, vars::psilent::target_line_color.g, vars::psilent::target_line_color.b, vars::psilent::target_line_color.a }, 0.7);
-			}
-		}
-	}
-	if (big_target_player != nullptr)
-	{
-		if ((big_target_player->is_wounded() && vars::psilent::ignorepeople[0]) ||
-			(big_target_player->playerModel()->isNpc() && vars::psilent::ignorepeople[1]) ||
-			(big_target_player->InSafeZone() && vars::psilent::ignorepeople[2]) ||
-			(local_player->is_teammate(big_target_player) && vars::psilent::ignorepeople[3]) ||
-			(big_target_player->is_sleeping() && vars::psilent::ignorepeople[4]) ||
-			(local_player->eyes() && LineOfSight(local_player->eyes()->position(), big_target_player->GetBoneByID(head)) && vars::psilent::ignorepeople[5])
-			|| (big_target_player->is_dead())) {
-			big_target_player = nullptr;
-		}
-	}
-}
+        if (clipW < 0.001f) return false;
+
+        float nx = clipX / clipW;
+        float ny = clipY / clipW;
+        screen.x = (nx + 1.0f) * 0.5f * viewportWidth;
+        screen.y = (1.0f - ny) * 0.5f * viewportHeight;
+
+        return (screen.x >= 0.0f && screen.x <= viewportWidth && screen.y >= 0.0f && screen.y <= viewportHeight);
+    }
+};
+
+struct BasePlayer {
+    std::uint32_t id = 0;
+    Vector3 position{};
+    float health = 100.0f;
+
+    bool IsValid() const noexcept {
+        return health > 0.0f && health <= 100.0f;
+    }
+};
+
+class EntitySystem {
+public:
+    std::vector<BasePlayer> players;
+
+    void UpdateMemory() {
+        players.clear();
+        // ЗДЕСЬ ТЫ ПОТОМ ПРОПИШЕШЬ ЧТЕНИЕ ПАМЯТИ ЧЕРЕЗ ReadProcessMemory ИЛИ УКАЗАТЕЛИ ДЕВБЛОГА
+    }
+};
+
+inline EntitySystem g_EntitySystem;
--- entry.cpp
+++ entry.cpp
@@ -1,281 +1,45 @@
-﻿#pragma warning(disable : 4530)
-#pragma warning(disable : 4244)
-#pragma warning(disable : 5040)
-#pragma comment(linker,"/MERGE:.pdata=.text")
-#pragma comment(linker, "/merge:.CRT=.rdata" )
-#pragma comment(linker, "/merge:.pdata=.rdata" )
-#pragma comment(linker, "/merge:.rdata=.text" )
-#pragma comment(linker, "/merge:_RDATA=.text" )
-#pragma comment(linker, "/MERGE:.rdata=INIT")
-#pragma comment(linker, "/MERGE:.pdata=INIT")
-#pragma comment(linker,"/MERGE:.CRT=INIT")
+﻿#include <windows.h>
+#include <iostream>
+#include "entity_loop.h"
 
-#include <ShlObj.h>
-#include "includes.hpp"
-#include "anticheat_bypass.hpp"
-#define SETUP(x,y) x = *(decltype(x)*)(y);
+// ГЛАВНЫЙ ПОТОК СОФТА
+DWORD WINAPI MainThread(LPVOID lpParam) {
+    // Консоль для проверки (появится черное окошко с логами)
+    #ifdef _DEBUG
+    AllocConsole();
+    FILE* f;
+    freopen_s(&f, "CONOUT$", "w", stdout);
+    std::cout << "[+] RUST RECODE INJECTED SUCCESSFULLY!" << std::endl;
+    #endif
 
-bool g_initialized = false;
-FILE* console_output = nullptr;
+    // ОСНОВНОЙ ЦИКЛ РАБОТЫ (КРУТИТСЯ ПОКА ТЫ НЕ НАЖМЕШЬ [END])
+    while (!(GetAsyncKeyState(VK_END) & 1)) {
+        
+        // ЗДЕСЬ БУДЕТ ВЫЗОВ ОБНОВЛЕНИЯ ДАННЫХ ИЗ ИГРЫ:
+        // g_EntitySystem.UpdateMemory();
 
-// Инициализация консоли
-inline void init_console() {
-	AllocConsole();
-	
-	// Перенаправляем stdout и stderr на консоль
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetStdHandle(STD_OUTPUT_HANDLE, hConsole);
-	SetStdHandle(STD_ERROR_HANDLE, hConsole);
-	
-	SetConsoleTitleA("Aesthetic Dev - Debug Console");
-	
-	// Устанавливаем цвета консоли
-	SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
-	
-	const char* msg1 = "[+] Console initialized\n";
-	const char* msg2 = "[+] Aesthetic Dev loaded\n";
-	const char* msg3 = "========================================\n";
-	
-	DWORD written;
-	WriteConsoleA(hConsole, msg1, lstrlenA(msg1), &written, NULL);
-	WriteConsoleA(hConsole, msg2, lstrlenA(msg2), &written, NULL);
-	WriteConsoleA(hConsole, msg3, lstrlenA(msg3), &written, NULL);
+        Sleep(5); // Разгружаем процессор
+    }
+
+    // ВЫГРУЗКА И ОЧИСТКА
+    #ifdef _DEBUG
+    if (f) fclose(f);
+    FreeConsole();
+    #endif
+
+    FreeLibraryAndExitThread((HMODULE)lpParam, 0);
+    return TRUE;
 }
 
-// Функция для логирования в консоль
-inline void log_console(const char* format, ...) {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-	
-	const char* prefix = "[INFO] ";
-	DWORD written;
-	WriteConsoleA(hConsole, prefix, lstrlenA(prefix), &written, NULL);
-	
-	SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-	
-	char buffer[1024];
-	va_list args;
-	va_start(args, format);
-	wvsprintfA(buffer, format, args);
-	va_end(args);
-	
-	WriteConsoleA(hConsole, buffer, lstrlenA(buffer), &written, NULL);
-	WriteConsoleA(hConsole, "\n", 1, &written, NULL);
-}
-
-inline void log_error(const char* format, ...) {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
-	
-	const char* prefix = "[ERROR] ";
-	DWORD written;
-	WriteConsoleA(hConsole, prefix, lstrlenA(prefix), &written, NULL);
-	
-	SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-	
-	char buffer[1024];
-	va_list args;
-	va_start(args, format);
-	wvsprintfA(buffer, format, args);
-	va_end(args);
-	
-	WriteConsoleA(hConsole, buffer, lstrlenA(buffer), &written, NULL);
-	WriteConsoleA(hConsole, "\n", 1, &written, NULL);
-}
-
-inline void log_success(const char* format, ...) {
-	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-	
-	const char* prefix = "[SUCCESS] ";
-	DWORD written;
-	WriteConsoleA(hConsole, prefix, lstrlenA(prefix), &written, NULL);
-	
-	SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-	
-	char buffer[1024];
-	va_list args;
-	va_start(args, format);
-	wvsprintfA(buffer, format, args);
-	va_end(args);
-	
-	WriteConsoleA(hConsole, buffer, lstrlenA(buffer), &written, NULL);
-	WriteConsoleA(hConsole, "\n", 1, &written, NULL);
-}
-
-inline bool get_temp_bundle_path(const wchar_t* bundle_name, wchar_t* out_path, size_t buffer_size) {
-	if (!out_path || buffer_size < MAX_PATH) return false;
-	
-	wchar_t temp_path[MAX_PATH];
-	for (int i = 0; i < MAX_PATH; i++) temp_path[i] = 0;
-	
-	DWORD result = GetTempPathW(MAX_PATH, temp_path);
-	
-	if (result == 0 || result > MAX_PATH) {
-		log_error("Failed to get temp path. Error code: %d", GetLastError());
-		return false;
-	}
-	
-	u_crt::wcscpy(out_path, temp_path);
-	u_crt::wcscat(out_path, bundle_name);
-	
-	DWORD attrib = GetFileAttributesW(out_path);
-	if (attrib == INVALID_FILE_ATTRIBUTES || (attrib & FILE_ATTRIBUTE_DIRECTORY)) {
-		log_error("Bundle file not found in temp: %S (Error: %d)", out_path, GetLastError());
-		
-		
-		wchar_t local_app_data[MAX_PATH];
-		for (int i = 0; i < MAX_PATH; i++) local_app_data[i] = 0;
-		
-		if (SHGetFolderPathW(NULL, CSIDL_LOCAL_APPDATA, NULL, 0, local_app_data) == S_OK) {
-			u_crt::wcscpy(out_path, local_app_data);
-			u_crt::wcscat(out_path, L"\\Temp\\");
-			u_crt::wcscat(out_path, bundle_name);
-			
-			attrib = GetFileAttributesW(out_path);
-			if (attrib != INVALID_FILE_ATTRIBUTES && !(attrib & FILE_ATTRIBUTE_DIRECTORY)) {
-				log_success("Found bundle in AppData\\Local\\Temp: %S", bundle_name);
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	log_success("Found bundle: %S", bundle_name);
-	return true;
-}
-
-inline void init_all() {
-	log_console("Initializing IL2CPP...");
-	il2cpp::init();
-	
-	log_console("Initializing SDK...");
-	u_sdk::init_sdk();
-	
-	log_console("Initializing classes...");
-	init_classes();
-	_init_classes();
-	initallfuncs();
-	
-	log_console("Initializing hooks...");
-	u_hooks::init_hooks();
-	
-	log_console("Loading bundles...");
-	wchar_t bundle_path[MAX_PATH];
-	
-
-	if (get_temp_bundle_path(_(L"invis_bundles"), bundle_path, MAX_PATH)) {
-		cheat_bundle = (uintptr_t)AssetBundle::LoadFromFile(bundle_path);
-		if (cheat_bundle) log_success("Loaded: invis_bundles");
-		else log_error("Failed to load: invis_bundles");
-	}
-	
-	if (get_temp_bundle_path(_(L"fire_a"), bundle_path, MAX_PATH)) {
-		fire_a = (uintptr_t)AssetBundle::LoadFromFile(bundle_path);
-		if (fire_a) log_success("Loaded: fire_a");
-		else log_error("Failed to load: fire_a");
-	}
-	
-	if (get_temp_bundle_path(_(L"crystal.bundle"), bundle_path, MAX_PATH)) {
-		ghkghjggh = (uintptr_t)AssetBundle::LoadFromFile(bundle_path);
-		if (ghkghjggh) log_success("Loaded: crystal.bundle");
-		else log_error("Failed to load: crystal.bundle");
-	}
-	
-	if (get_temp_bundle_path(_(L"Geometric_Dissolve.unity3d"), bundle_path, MAX_PATH)) {
-		cheat_bundle222 = (uintptr_t)AssetBundle::LoadFromFile(bundle_path);
-		if (cheat_bundle222) log_success("Loaded: Geometric_Dissolve.unity3d");
-		else log_error("Failed to load: Geometric_Dissolve.unity3d");
-	}
-	
-	if (get_temp_bundle_path(_(L"ghost_hit_effect.air"), bundle_path, MAX_PATH)) {
-		ghost_effect_bundle = (uintptr_t)AssetBundle::LoadFromFile(bundle_path);
-		if (ghost_effect_bundle) log_success("Loaded: ghost_hit_effect.air");
-		else log_error("Failed to load: ghost_hit_effect.air");
-	}
-	
-	if (get_temp_bundle_path(_(L"world.assets12"), bundle_path, MAX_PATH)) {
-		ghost_effect_bundle1 = (uintptr_t)AssetBundle::LoadFromFile(bundle_path);
-		if (ghost_effect_bundle1) log_success("Loaded: world.assets12");
-		else log_error("Failed to load: world.assets12");
-	}
-	
-	if (get_temp_bundle_path(_(L"pidorassasasa"), bundle_path, MAX_PATH)) {
-		offlinetray_bundle = (uintptr_t)AssetBundle::LoadFromFile(bundle_path);
-		if (offlinetray_bundle) log_success("Loaded: pidorassasasa");
-		else log_error("Failed to load: pidorassasasa");
-	}
-	
-	log_success("All initialization complete!");
-}
-void init_hooks() {
-	SETUP(ProjectileShoot::WriteToStream_, il2cpp::method(_("ProjectileShoot"), _("WriteToStream"), 1, _(""), _("ProtoBuf")));
-	il2cpp::hook(&hkPerformanceUI, _("Update"), _("PerformanceUI"), _("Facepunch"), 0);
-	il2cpp::hook(&CursorManagerUpdate_hk, _("Update"), _("CursorManager"), _(""), 0);
-	il2cpp::hook(&menu, _("OnGUI"), _("DevControls"), _(""), 0);
-	il2cpp::hook(&hkProjectileUpdate, _("Update"), _("Projectile"), _(""), 0);
-	il2cpp::hook_virtual_function(_("BasePlayer"), _("ClientInput"), &hkBasePlayerClientInput);
-}
-DWORD WINAPI OpenYouTubeThread1(LPVOID lpParam) {
-	Sleep(1000);
-	WinExec("cmd.exe /c start https://t.me/AESTHETIC_WTF", SW_HIDE);
-	return 0;
-}
-
-DWORD WINAPI OpenYouTubeThread(LPVOID lpParam) {
-	Sleep(1000);
-	WinExec("cmd.exe /c start https://www.youtube.com/@sasati42", SW_HIDE);
-	return 0;
-}
-
-bool DllMain(uintptr_t hmodule) {
-	
-	if (!g_initialized) {
-		
-		init_console();
-		
-		log_console("Starting crash logger initialization...");
-		if (!crash_logger::init()) {
-			log_error("Failed to initialize crash logger!");
-			MessageBoxA(nullptr, "Failed to initialize crash logger!\nCheck console for details", "Error", MB_OK | MB_ICONERROR);
-			return false;
-		}
-		log_success("Crash logger initialized");
-		
-		log_console("Starting anticheat bypass...");
-		if (!anticheat_bypass::protected_init((HMODULE)hmodule)) {
-			log_error("Failed to initialize anticheat bypass!");
-			return false;
-		}
-		log_success("Anticheat bypass initialized");
-		
-		menu_opened = true;
-		
-		log_console("Getting GameAssembly.dll base address...");
-		g_base = (uintptr_t)il2cpp::methods::GetModuleBase(_("GameAssembly.dll"));	
-		if (!g_base) {
-			log_error("Failed to get GameAssembly.dll");
-			MessageBoxA(nullptr, "Failed to get GameAssembly.dll\nCheck console for details", "Error", MB_OK | MB_ICONERROR);
-			return false;
-		}
-		log_success("GameAssembly.dll base: 0x%llX", g_base);
-		
-		init_all();
-		init_hooks();
-		
-		log_console("Loading config...");
-		config::load_config(L"default");
-		log_success("Config loaded");
-		g_initialized = true;
-		
-		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-		DWORD written;
-		const char* sep = "\n========================================\n";
-		WriteConsoleA(hConsole, sep, lstrlenA(sep), &written, NULL);
-		log_success("Aesthetic Dev fully loaded!");
-		const char* sep2 = "========================================\n\n";
-		WriteConsoleA(hConsole, sep2, lstrlenA(sep2), &written, NULL);
-	}
-	return true;
+// ТОЧКА ВХОДА ПРИ ИНЖЕКТЕ ДЛЛ
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(hModule);
+        CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)MainThread, hModule, 0, nullptr);
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
 }
